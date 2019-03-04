cdd51199c587cca67a2287da79a3c4db35b01860
k7996g <35267235+k7996g@users.noreply.github.com>
2019-03-04 15:31:21 +0530

    Add files via upload

---
+++ ABC/src/com/cg/springboot/bean/Client.java
@@ -0,0 +1,18 @@
+package com.cg.springboot.bean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan("com.cg")
+public class Client {
+	public static void main(String args[]) {
+		ApplicationContext context = SpringApplication.run(Client.class, args);
+		Employee emp = (Employee) context.getBean("emp");
+		System.out.println(emp);
+	}
+}
